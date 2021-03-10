2ef12c2b9951bb213761a9299a97815516ff037b
MariaGrecia <mgcutipag@eservicios.indracompany.com>
2021-03-10 18:17:17 -0500

    config: scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mg-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'bcp-challenge';
